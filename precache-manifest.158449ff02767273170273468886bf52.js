3ba3119336512a6d7f23b606033edd430ca78705
Victor <victor.shevchuk.96@gmail.com>
2022-01-09 13:15:27 +0200

    Updates

---
+++ precache-manifest.158449ff02767273170273468886bf52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2b46cdae93e3f1f776884580a4a59b",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "33d42cf905f8d34645d7",
+    "url": "/todos/static/css/main.1213836f.chunk.css"
+  },
+  {
+    "revision": "675f19ed15dd49eb13be",
+    "url": "/todos/static/js/2.cb4f7fb6.chunk.js"
+  },
+  {
+    "revision": "8d98233dfb2f1a8b874a10cb1448b7bd",
+    "url": "/todos/static/js/2.cb4f7fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33d42cf905f8d34645d7",
+    "url": "/todos/static/js/main.be3b1225.chunk.js"
+  },
+  {
+    "revision": "2e71935c59c1caec78b4",
+    "url": "/todos/static/js/runtime-main.c2bc5e4b.js"
+  },
+  {
+    "revision": "62038fcbf10a494db9234247808b39d0",
+    "url": "/todos/static/media/times-solid.62038fcb.svg"
+  }
+]);
